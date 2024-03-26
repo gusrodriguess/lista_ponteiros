9d6f6318ac99e61fed11f921a296f99cf0a369af
Gustavo Rodrigues <90390443+gusrodriguess@users.noreply.github.com>
2024-03-25 21:26:35 -0300

    Update questao5.c

--- questao5.c
+++ questao5.c
@@ -8,22 +8,22 @@ int main(void){
   f = vet;
   printf("contador/valor/valor/endereco/endereco\n");
   for(i = 0 ; i <= 4 ; i++){
-  // Valor de i (valor de cada iteração do laço)
+  // Valor de i (valor de cada iteraÃ§Ã£o do laÃ§o)
     printf("i = %d",i);
 
-  // Valor contido na posição i do vetor.
+  // Valor contido na posiÃ§Ã£o i do vetor.
     printf(" vet[%d] = %.1f",i, vet[i]);
 
-  /* Conteúdo do endereço de (f + (0, 4, 8, 12, 16)) bytes, ou seja, a cada incremento
-   o ponteiro pula  4 bytes, que é o tamanho do tipo de dado que ele aponta, que no caso
-   apresentado o tipo é float
+  /* ConteÃºdo do endereÃ§o de (f + (0, 4, 8, 12, 16)) bytes, ou seja, a cada incremento
+   o ponteiro pula  4 bytes, que Ã© o tamanho do tipo de dado que ele aponta, que no caso
+   apresentado o tipo Ã© float
   */
     printf(" *(f + %d) = %.1f",i, *(f+i));
 
-  // O endereço de memória do valor contido na posição i do vetor.
+  // O endereÃ§o de memÃ³ria do valor contido na posiÃ§Ã£o i do vetor.
     printf(" &vet[%d] = %X",i, &vet[i]);
 
-  /* O endereço de memória de f, mas a cada incremento esse endereço
+  /* O endereÃ§o de memÃ³ria de f, mas a cada incremento esse endereÃ§o
   pula 4 bytes, que o tamanho de um valor do tipo float */
     printf(" (f + %d) = %X",i, f+i);
 
@@ -33,7 +33,7 @@ int main(void){
 }
 
 /*
-    Saída do programa:
+    SaÃ­da do programa:
 
     i = 0 vet[0] = 1.1 *(f + 0) = 1.1 &vet[0] = 61FDF0 (f + 0) = 61FDF0
     i = 1 vet[1] = 2.2 *(f + 1) = 2.2 &vet[1] = 61FDF4 (f + 1) = 61FDF4
@@ -41,10 +41,10 @@ int main(void){
     i = 3 vet[3] = 4.4 *(f + 3) = 4.4 &vet[3] = 61FDFC (f + 3) = 61FDFC
     i = 4 vet[4] = 5.5 *(f + 4) = 5.5 &vet[4] = 61FE00 (f + 4) = 61FE00
 
-    Os valores mostrados são exatamente o que era esperado, pois quando o que é mostrado
-    é o contéudo, a cada incremento o ponteiro pula 4 bytes no endereço e mostra o valor
-    guardado naquele endereço, da mesma forma é quando é mostrado o endereço, a cada incremento
-    é somado ('4 bytes' * 'o valor de i') no endereço base, como podemos ver que na iteração 1, 7
-    o endereço que antes era 61FDF0 mudou para 61FDF4.
+    Os valores mostrados sÃ£o exatamente o que era esperado, pois quando o que Ã© mostrado
+    Ã© o contÃ©udo, a cada incremento o ponteiro pula 4 bytes no endereÃ§o e mostra o valor
+    guardado naquele endereÃ§o, da mesma acontece quando Ã© mostrado o endereÃ§o, a cada incremento
+    Ã© somado ('4 bytes' * 'o valor de i'), como podemos ver que na iteraÃ§Ã£o 1, o endereÃ§o que 
+    antes era 61FDF0 mudou para 61FDF4.
 */
 
